a1322d4456c397c776b24a9d385cb60cdb564302
snady <sf373@cornell.edu>
2018-05-01 19:02:26 -0400

    draw a fake board

--- draw.ml
+++ draw.ml
@@ -5,6 +5,8 @@ let document = Html.window##.document
 let pi = acos (-1.)
 
 let bg_color = jstr "#BDC3C7"
+let brdr_color = jstr "#af7418"
+let pool_color = jstr "#0a6c03"
 let ball_color = jstr "red"
 
 
@@ -13,8 +15,19 @@ let draw_background canvas =
   let cw = float canvas##.width in
   let ch = float canvas##.height in
     ctx##rect 0. 0. cw ch;
-    ctx##.fillStyle := bg_color;
+    ctx##.fillStyle := pool_color;
     ctx##fill
+
+let draw_board canvas =
+  let ctx = canvas##getContext (Html._2d_) in
+  let cw = float canvas##.width in
+  let ch = float canvas##.height in
+  let border_rad = 20. in
+    ctx##.fillStyle := jstr("brown");
+    ctx##fillRect 0. 0. border_rad ch;
+    ctx##fillRect 0. 0. cw border_rad;
+    ctx##fillRect 0. (ch-.border_rad) cw border_rad;
+    ctx##fillRect (cw-.border_rad) 0. border_rad ch
 
 let draw_ball canvas =
   let ctx = canvas##getContext (Html._2d_) in
@@ -22,8 +35,7 @@ let draw_ball canvas =
   let by = 128. in
   let brad = 16. in
     ctx##beginPath;
-    ctx##rect bx by brad brad;
-    (* ctx##arc bx by brad 0. pi true; *)
+    ctx##arc bx by brad 0. (2.*.pi) Js._true;
     ctx##.strokeStyle := ball_color;
     ctx##stroke
 
@@ -36,6 +48,7 @@ let start _ =
         Printf.printf "Cant find canvas \n";
         assert false ) in
   draw_background canvas;
+  draw_board canvas;
   draw_ball canvas
 
 
