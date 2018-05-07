09f39b919da3d5d2bc709b336cdbfb67e855da2d
snady <sf373@cornell.edu>
2018-05-07 03:40:29 -0400

    styling board also showing ball type

--- draw.ml
+++ draw.ml
@@ -18,6 +18,8 @@ let solid_color = jstr "#C3272B"
 
 let init_pos = eight_ball_init_ball_pos
 
+let table_off = 76.
+
 let test_ball =
   match init_pos with
   | [] -> failwith "Empty"
@@ -27,11 +29,10 @@ let (tbx, tby) = get_position test_ball
 
 let draw_background canvas =
   let ctx = canvas##getContext (Html._2d_) in
-  let cw = float canvas##.width in
-  let ch = float canvas##.height in
-    ctx##rect 0. 0. cw ch;
-    ctx##.fillStyle := pool_color;
-    ctx##fill
+  let tablesrc = jstr "img/table-scaled.png" in
+  let tableimg = Html.createImg document in
+  tableimg##.src := tablesrc;
+  ctx##drawImage tableimg 0. 0.
 
 let draw_board canvas =
   let ctx = canvas##getContext (Html._2d_) in
@@ -46,27 +47,32 @@ let draw_board canvas =
 
 let draw_ball canvas ball off =
   let ctx = canvas##getContext (Html._2d_) in
+  let t_o = table_off in
     let (bx, by) = get_position ball in
+    let (bx, by) = (bx+.t_o, by+.t_o) in
     let brad = 11.4 in
-    ctx##beginPath;
+    let ballsrc = jstr (get_color ball) in
+    let ballimg = Html.createImg document in
+    ballimg##.src := ballsrc;
+    ctx##drawImage_withSize ballimg (bx-.brad) (by-.brad) (2.*.brad) (2.*.brad)
+    (* ctx##beginPath;
     ctx##arc bx by brad 0. (2.0*.pi) Js._true;
     ctx##.fillStyle := ball_color;
-    ctx##fill
+    ctx##fill *)
 
 let draw_state canvas =
   List.iter (fun b -> draw_ball canvas b 0. ) init_pos
 
 let draw canvas off =
   draw_background canvas;
-  draw_board canvas;
   draw_state canvas;
   ()
 
 let rec start _ =
-  let main =
+(*   let main =
     Js.Opt.get (document##getElementById (jstr "main"))
       (fun () -> assert false)
-  in
+  in *)
   let canvas =
     Js.Opt.get
       (Js.Opt.bind ( Html.document##getElementById (jstr "canvas"))
